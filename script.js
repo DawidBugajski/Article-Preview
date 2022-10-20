0694834831528156a1659f53140895e0cb067872
Dawid Bugajski <dawidbugajski1996@gmail.com>
2022-10-20 10:33:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".user__btn");
+const userActive = document.querySelector(".user__active");
+const btnClassess = btn.classList;
+
+btn.addEventListener("click", () => {
+  btnClassess.toggle("active");
+  active();
+});
+
+// * change z-index && opacity of the element
+const active = () => {
+  let popup = btnClassess.contains("active")
+    ? (userActive.style.zIndex = "2")
+    : (userActive.style.zIndex = "-1");
+
+  let opacity =
+    userActive.style.zIndex === "-1"
+      ? (userActive.style.opacity = "0")
+      : (userActive.style.opacity = "1");
+};
